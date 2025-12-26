a81ee445b39a737a87fb402a08e36527495df979
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2025-12-26 18:16:09 -0300

    Update avaliacao.js

--- js/avaliacao.js
+++ js/avaliacao.js
@@ -217,13 +217,14 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     resultado.style.display = "block";
     resultado.className = "resultado resultado-" + classe;
-resultado.innerHTML = `
-  <div class="selo">${status}</div>
-  <strong style="display: inline;">ID:</strong> ${dados.id}<br>
-  <strong style="display: inline;">Pontuação:</strong> ${pontuacao}<br>
-  <strong style="display: inline;">Avaliador:</strong> ${dados.avaliador}<br>
-  ${navigator.onLine ? "☁️ Enviado ao sistema" : "📴 Salvo offline"}
-`;
+    resultado.innerHTML = `
+      <div class="selo">${status}</div><br>
+      <strong style="display: inline;">ID:</strong> ${dados.id}<br>
+      <strong style="display: inline;">Pontuação:</strong> ${pontuacao}<br>
+      <strong style="display: inline;">Avaliador:</strong> ${dados.avaliador}<br>
+      ${navigator.onLine ? "☁️ Enviado ao sistema" : "📴 Salvo offline"}
+    `;
+
 
 
     try{
