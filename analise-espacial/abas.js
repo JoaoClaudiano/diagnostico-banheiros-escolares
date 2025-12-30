3db705b89f0ebd358b0c4963d265884e71a75716
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2025-12-29 23:45:14 -0300

    Fix event target reference in abrirAba function

--- analise-espacial/abas.js
+++ analise-espacial/abas.js
@@ -1,7 +1,9 @@
+
 function abrirAba(id) {
   document.querySelectorAll(".secao").forEach(s => s.classList.remove("ativa"));
   document.querySelectorAll(".abas button").forEach(b => b.classList.remove("ativa"));
   document.getElementById(id).classList.add("ativa");
   event.target.classList.add("ativa");
 }
+
 
